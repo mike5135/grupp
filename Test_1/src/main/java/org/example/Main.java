1d317d756c6c641b6f495505515612d838632a21
mike5135 <micpap@kth.se>
2024-09-04 21:04:02 +0200

    etst14

--- Test_1/src/main/java/org/example/Main.java
+++ Test_1/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] a = {0, 1, 2, 3, 4, 5, 6, 7, 9};
+
+        for(int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+    // Metod för att returnera arrayen
+    public static int[] getArray() {
+        return new int[]{0, 1, 2, 3, 4, 5, 6, 8, 9};
     }
 }
--- Test_1/src/test/java/org/example/MainTest.java
+++ Test_1/src/test/java/org/example/MainTest.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
+
 public class MainTest {
+
+    @Test
+    public void testArrayContents() {
+        // Hämtar arrayen från Main klassen
+        int[] actualArray = Main.getArray();
+
+        // Förväntad array
+        int[] expectedArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        // Testar om den faktiska arrayen är lika med den förväntade arrayen
+        assertArrayEquals(expectedArray, actualArray, "Arrayen är inte som förväntat!");
+    }
 }
